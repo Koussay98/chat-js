a4162dead1e305a13d285acc1906484486e1022c
wael laataoui <laataouiwael@gmail.com>
2020-12-24 14:18:25 +0100

    wired up signin and display messages correctly

--- public/script.js
+++ public/script.js
@@ -1,34 +1,60 @@
+var name = localStorage.getItem("name");
+console.log(name, typeof (name));
+if (!name || name === "null") window.location.replace("/sign-in.html")
 var chatSocket = io('/chat')
+$('#name').val(name)
+$(document).ready(function () {
 
-$(document).ready(function() {
   getMessages();
-  $('#modal').modal('show')
+  // $('#modal').modal('show')
+})
+$("#logout").on("click", (e) => {
+  name = "";
+  localStorage.setItem("name", "")
+  window.location.replace("/sign-in.html")
 })
 
-$('#chatNameForm').on('submit', function(e) {
+
+$('#chatNameForm').on('submit', function (e) {
   e.preventDefault()
-  var name = $('#chatName').val()
-  
+  // var name = $('#chatName').val() //hook this
   $('#name').val(name)
   getMessages()
 })
 
-function updateFeed (message, method) {
-  var newMessage = `<div class="d-flex justify-content-end mb-4">
-                    <div class="msg_cotainer_send">
-                      ${message.message}
-                    </div>
-                    <div class="img_cont_msg">
-                      <span>${message.name}</span>
-                      <img src="" class="rounded-circle user_img_msg">
-                    </div>
-                  </div>`;
-                    
+function updateFeed(message, method) {
+  let newMessage;
+  if (message.name == name)
+    newMessage = `<div class="msg d-flex justify-content-end mb-4' ">
+    <div class="msg_cotainer_send"} >
+      ${message.message}
+    </div>
+    <span style="align-self:center;">${message.name}</span>
+    <div class="img_cont_msg">
+    <img src="user-alt-solid.svg" class="rounded-circle user_img_msg">
+    </div>
+    </div>`
+  else
+    newMessage = `<div class="d-flex justify-content-start mb-4">
+        <div class="img_cont_msg">
+          <img src="user-alt-solid.svg" class="rounded-circle user_img_msg">
+        </div>
+        <span style="align-self:center;">${message.name}</span>
+        <div class="msg_cotainer">
+        ${message.message}
+        </div>
+      </div>`
+
+
+
   if (method === 'append') {
     $('.chat-feed').append(newMessage)
   } else if (method === 'prepend') {
     $('.chat-feed').prepend(newMessage)
-  } 
+
+  }
+  $(".chat-feed").scrollTop($('.chat-feed')[0].scrollHeight)
+
 }
 
 function getMessages() {
@@ -36,17 +62,17 @@ function getMessages() {
     type: 'GET',
     dataType: 'json',
     url: '/api/chat',
-    success: function(response) {
+    success: function (response) {
       if (response.messages) {
-        response.messages.reverse().map(function(message){
+        response.messages.reverse().map(function (message) {
           updateFeed(message, 'prepend')
           $('#modal').modal('hide')
         })
       } else {
         $('#modal').modal('hide')
       }
-      
-      chatSocket.on('chat', function(message) {
+
+      chatSocket.on('chat', function (message) {
         message = JSON.parse(message)
         updateFeed(message, 'append')
       })
@@ -54,7 +80,7 @@ function getMessages() {
   })
 }
 
-$('#chatForm').on('submit', function(e) {
+$('#chatForm').on('submit', function (e) {
   e.preventDefault()
   var data = $('#chatForm').serialize()
   $.ajax({
@@ -63,5 +89,5 @@ $('#chatForm').on('submit', function(e) {
     processData: false,
     data: data
   })
-  console.log("sucess");
+  $("#message").val("")
 })
--- public/sign-in.js
+++ public/sign-in.js
@@ -2,6 +2,7 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('ctn');
 const navbar = document.getElementById('navbar');
+const loginForm = document.getElementById("signin-form");
 
 signUpButton.addEventListener('click', () => {
   container.classList.add("right-panel-active");
@@ -12,3 +13,10 @@ signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
   navbar.classList.remove("orangee");
 });
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const username = document.querySelector("input[name='username']").value
+  console.log(username);
+  localStorage.setItem("name", username)
+  window.location.replace("/")
+})
